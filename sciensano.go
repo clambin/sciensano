e5dd4d1fcde1019b4cefce22f07cb40426f19e83
Christophe Lambin <christophe.lambin@gmail.com>
2021-03-22 00:54:32 +0100

    Table Query for delivered vaccines

--- sciensano.go
+++ sciensano.go
@@ -10,6 +10,6 @@ import (
 func main() {
 	log.WithField("version", version.BuildVersion).Info("sciensano API starting")
 	handler, _ := apihandler.Create()
-	server := grafana_json.Create(handler.Endpoints, 8088)
+	server := grafana_json.Create(handler.Endpoints, 8080)
 	_ = server.Run()
 }
