284a6da66f753370214f0cc26347a59051572032
clambin <christophe.lambin@gmail.com>
2021-08-10 17:51:03 +0200

    feat: updated demographics

--- demographics/demographics.go
+++ demographics/demographics.go
@@ -9,20 +9,20 @@ func GetAgeGroupFigures() (groups map[string]int) {
 		"16-17": 253617,
 		"18-24": 922803,
 		"25-34": 1486440,
-		"35-44": 1485793,
-		"45-54": 1558559,
-		"55-64": 1523475,
-		"65-74": 1170399,
-		"75-84": 698940,
-		"85+":   335139,
+		"35-44": 1493556,
+		"45-54": 1538889,
+		"55-64": 1538307,
+		"65-74": 1194091,
+		"75-84": 703107,
+		"85+":   331923,
 	}
 }
 
 func GetRegionFigures() (groups map[string]int) {
 	return map[string]int{
-		"Flanders":   6629143,
-		"Wallonia":   3645243,
-		"Brussels":   1218255,
+		"Flanders":   6653062,
+		"Wallonia":   3570257,
+		"Brussels":   1219970,
 		"Ostbelgien": 77949,
 	}
 }
