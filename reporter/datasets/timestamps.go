720678c516f0bdbc5bc7a8c9fb40ced92fefcd88
clambin <christophe.lambin@gmail.com>
2022-03-27 15:24:36 +0200

    refactor: minor optimization

--- reporter/datasets/timestamps.go
+++ reporter/datasets/timestamps.go
@@ -40,7 +40,7 @@ func (t *Timestamps) Add(timestamp time.Time) (index int, added bool) {
 	index = len(t.timestamps)
 	t.timestampIndex[timestamp] = index
 
-	mustSort := len(t.timestamps) > 0 && timestamp.After(t.timestamps[len(t.timestamps)-1]) == false
+	mustSort := index > 0 && timestamp.After(t.timestamps[index-1]) == false
 	t.timestamps = append(t.timestamps, timestamp)
 	if mustSort {
 		sort.Slice(t.timestamps, func(i, j int) bool { return t.timestamps[i].Before(t.timestamps[j]) })
