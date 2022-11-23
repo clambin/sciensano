64a89c800b7ac0386a1fb43d235ce9d3c8dc7e17
clambin <christophe.lambin@gmail.com>
2022-11-23 18:22:20 +0100

    refactor: simplify

--- simplejsonserver/server_test.go
+++ simplejsonserver/server_test.go
@@ -2,6 +2,8 @@ package simplejsonserver
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/clambin/sciensano/cache/sciensano"
 	mockDemographics "github.com/clambin/sciensano/demographics/mocks"
 	"github.com/clambin/simplejson/v3"
 	"github.com/clambin/simplejson/v3/common"
@@ -11,6 +13,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 	"time"
@@ -23,6 +27,11 @@ func TestNew(t *testing.T) {
 
 	h, err := New(0, demographicsClient)
 	require.NoError(t, err)
+	h.apiCache.Cases.Fetcher = &fetcher[sciensano.Cases]{}
+	h.apiCache.Hospitalisations.Fetcher = &fetcher[sciensano.Hospitalisations]{}
+	h.apiCache.Mortalities.Fetcher = &fetcher[sciensano.Mortalities]{}
+	h.apiCache.TestResults.Fetcher = &fetcher[sciensano.TestResults]{}
+	h.apiCache.Vaccinations.Fetcher = &fetcher[sciensano.Vaccinations]{}
 
 	ctx := context.Background()
 
@@ -76,4 +85,38 @@ func TestNew(t *testing.T) {
 	assert.Equal(t, http.StatusOK, b.Code)
 	assert.Contains(t, b.Body.String(), `"Handlers": `)
 	assert.Contains(t, b.Body.String(), `"ReporterCache": `)
+}
+
+type fetcher[T any] struct {
+}
+
+func (f fetcher[T]) GetLastModified(_ context.Context) (time.Time, error) {
+	return time.Now(), nil
+}
+
+func (f fetcher[T]) Fetch(_ context.Context) (T, error) {
+	var t T
+	input, err := os.Open(filepath.Join("..", "cache", "sciensano", "input", f.GetTarget()+".json"))
+	if err == nil {
+		err = json.NewDecoder(input).Decode(&t)
+		_ = input.Close()
+	}
+	return t, err
+}
+
+func (f fetcher[T]) GetTarget() (target string) {
+	var t T
+	switch interface{}(t).(type) {
+	case sciensano.Cases:
+		target = "cases"
+	case sciensano.Hospitalisations:
+		target = "hospitalisations"
+	case sciensano.Mortalities:
+		target = "mortalities"
+	case sciensano.TestResults:
+		target = "testResults"
+	case sciensano.Vaccinations:
+		target = "vaccinations"
+	}
+	return target
 }
